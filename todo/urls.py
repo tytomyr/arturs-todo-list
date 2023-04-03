c8688f08ebab5595d44b76f1e474347e1dd1d0dd
Artur Tytomyr <art.tytomyr@gmail.com>
2023-04-03 11:20:13 +0300

    html files edited, sidebar and pagination added

--- todo/urls.py
+++ todo/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
-from .views import (index,
-                    TagListView,
+from .views import (TagListView,
                     TaskListView)
 
 urlpatterns = [
     path("", TaskListView.as_view(), name="index"),
-    path("admin/", index, name="admin"),
+    path("admin/", TaskListView.as_view(), name="admin"),
     path("tags/", TagListView.as_view(), name="tags"),
 ]
 
--- todo/views.py
+++ todo/views.py
@@ -5,17 +5,6 @@ from todo.models import Tag, Task
 from todo.forms import TagForm, TaskForm
 
 
-def index(request):
-    num_tasks = Task.objects.count()
-    num_tags = Tag.objects.count()
-
-    context = {
-        "num_tasks": num_tasks,
-        "num_tags": num_tags,
-    }
-    return render(request, "todo/index.html", context=context)
-
-
 class TagListView(generic.ListView):
     model = Tag
     form_class = TagForm
@@ -24,3 +13,4 @@ class TagListView(generic.ListView):
 class TaskListView(generic.ListView):
     model = Task
     form_class = TaskForm
+    template_name = "todo/index.html"
